74be6d96d589fe3654116aa2cca59cf1ffa7f92d
infolizocity-collab <info.lizocity@gmail.com>
2026-03-24 23:50:41 +0530

    remove dotenv

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-// Load environment variables
-require("dotenv").config();
+
 
 // Import dependencies
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-// ✅ FIXED PATH
+// Routes
 const earnRoutes = require("./src/routes/earnRoutes");
 const referralRoutes = require("./src/routes/referralRoutes");
 
-// Routes
 app.use("/api/referral", referralRoutes);
 app.use("/api/earn", earnRoutes);
 
 // PORT
 const PORT = process.env.PORT || 5000;
+
+// DEBUG (IMPORTANT 🔥)
+console.log("APP_ID:", process.env.APP_ID);
+console.log("SECRET_KEY:", process.env.SECRET_KEY);
 
 // DB
 connectDB();
