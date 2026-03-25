a8b1c3000453c55043a38c6619c2558042b2d985
infolizocity-collab <info.lizocity@gmail.com>
2026-03-25 19:31:10 +0530

    fix server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// Load environment variables
 require("dotenv").config();
 
 // Import dependencies
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-// ✅ FIXED PATH
+// Routes
 const earnRoutes = require("./src/routes/earnRoutes");
 const referralRoutes = require("./src/routes/referralRoutes");
 const withdrawalRoutes = require("./src/routes/withdrawalRoutes");
 
-// Routes
+// Register Routes
 app.use("/api/referral", referralRoutes);
 app.use("/api/earn", earnRoutes);
 app.use("/api/withdrawals", withdrawalRoutes);
@@ -18,10 +17,10 @@ app.use("/api/withdrawals", withdrawalRoutes);
 // PORT
 const PORT = process.env.PORT || 5000;
 
-// DB
+// DB connect
 connectDB();
 
 // Server start
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🔥 Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
